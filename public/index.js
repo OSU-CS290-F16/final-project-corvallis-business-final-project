1ba8da4818741c7a0873e2a488b8ad99412b59df
Hampus Hammarlund <hampus_hammarlund@hotmail.com>
2016-12-04 22:38:23 -0800

    Add place half way done, image downloads but no json edit yet, will do later

--- public/index.js
+++ public/index.js
@@ -1,1 +1,73 @@
 //index file
+
+function createNewPlace(name, type, address, phone, description, url, callback){
+    //console.log("here");
+    // We'll post to the add-photo endpoint for the appropriate person.
+    var postUrl = '/create/';
+
+    // Start a new request to post our newly added photo as JSON data.
+    var postRequest = new XMLHttpRequest();
+    postRequest.open('POST', postUrl);
+    postRequest.setRequestHeader('Content-Type', 'application/json');
+
+    /*
+    * Set up a simple handler for completed requests.  This will send an error
+    * into the callback if we don't get a 200 (success) status back.
+    */
+    postRequest.addEventListener('load', function (event) {
+        var error;
+        if (event.target.status !== 200) {
+            error = event.target.response;
+        }
+        callback(error);
+    });
+
+    // Send our photo data off to the server.
+    postRequest.send(JSON.stringify({
+        name: name,
+        type: type,
+        address: address,
+        phone: phone,
+        description: description,
+        url: url
+    }));
+}
+
+function addNewPlace(){
+
+    var name = document.getElementById('input-business-name').value || '';
+    var type = document.getElementById('input-business-type').value || '';
+    var address = document.getElementById('input-business-address').value || '';
+    var phone = document.getElementById('input-business-phone').value || '';
+    var description = document.getElementById('input-business-description').value || '';
+    var url = document.getElementById('input-business-url').value || '';
+
+    //console.log("url " + url);
+
+    if(name.trim() && type.trim() && address.trim() && phone.trim() && description.trim() && url.trim() ){
+    //if(url.trim()){
+        //console.log("url " + url);
+        //all fields are there
+        //alert("completed");
+        createNewPlace(name, type, address, phone, description, url, function(err){
+            if(err){
+                //print error to screen
+                alert("Error:\n" + err);
+            } else {
+                //redirect to new page
+                alert("did the thing!");
+            }
+        });
+    } else {
+        alert("Please complete all input forms");
+    }
+    //alert("done");
+
+}
+
+document.addEventListener('DOMContentLoaded', function (event) {
+    var addNewPlaceButton = document.getElementById('add-new-place-button');
+    if(addNewPlaceButton){
+        addNewPlaceButton.addEventListener('click', addNewPlace);
+    }
+});
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
+var fs = require('fs');
+var request = require('request');
+var bodyParser = require('body-parser');
 
 var app = express();
 var placeData = require('./place-data');
@@ -16,6 +19,9 @@ app.set('view engine', 'handlebars');
 *
 */
 
+//parse json
+app.use(bodyParser.json());
+
 //static files from /public
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -30,15 +36,15 @@ app.get('/', function (req, res) {
 
 //for '/place/NAME'
 app.get('/place/:place', function (req, res, next) {
-	
-	// Fetches based on JSON object's name, not name property
-	var place = placeData[req.params.place];
+
+    // Fetches based on JSON object's name, not name property
+    var place = placeData[req.params.place];
 
     if(place){
-		//console.log("tested success");
+        //console.log("tested success");
         var placeName = place.name;
         res.status(200).render('place-page', {
-			pageTitle: 'Place Page - ' + placeName, //placeName
+            pageTitle: 'Place Page - ' + placeName, //placeName
             place: place,
             placeName: placeName
         });
@@ -55,12 +61,68 @@ app.get('/search', function (req, res) {
     });
 });
 
-//for '/new' - search page     
+//for '/new' - search page
 app.get('/new', function (req, res) {
     res.status(200).render('new-page', {
         title: 'New title',
         placeData: placeData
     });
+});
+
+app.post('/create/', function (req, res, next) {
+    if(req.body && req.body.url){
+        var varName = req.body.name;
+        var varType = req.body.type;
+        var varPhone = req.body.phone;
+        var varAddress = req.body.address;
+        var varDescription = req.body.description;
+
+
+        //download and save image in correct place
+        var download = function(uri, filename, callback){
+            request.head(uri, function(err, res, body){
+                console.log('content-type:', res.headers['content-type']);
+                console.log('content-length:', res.headers['content-length']);
+
+                request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+            });
+        };
+
+        download(req.body.url, "public/images/" + req.body.name + ".jpg", function(){
+            console.log('done');
+        });
+
+        //add to json
+        /*var temp = JSON.stringify({
+            name: varName,
+            type: varType,
+            phone: varPhone,
+            address: varAddress,
+            city: "Corvallis",
+            zipcode: "97330",
+            image: "public/images/" + req.body.name + ".jpg",
+            description: varDescription,
+            image_courtesy: "Nan"
+        });*/
+/*
+        placeData.varName = varName;
+
+        fs.writeFile("place-data.json", JSON.stringify(placeData, null, 2), function (err) {
+            if (err) return console.log(err);
+            console.log(JSON.stringify(placeData));
+            console.log('writing to ' + "place-data.json");
+        });
+*/
+        //var obj = JSON.parse(fs.readFileSync('place-data.json', 'utf8'));
+        //placeData.push(newEntry);
+
+        //send success message
+        res.status(200).send();
+    } else {
+        res.status(400).send("No url");
+    }
+
+
 });
 
 //404
