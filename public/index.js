fd3fd87f891a62189da9a167834a3efaba8a1f2f
Hampus Hammarlund <hampus_hammarlund@hotmail.com>
2016-12-04 23:28:03 -0800

    Couple edits to creation

--- public/index.js
+++ public/index.js
@@ -1,19 +1,12 @@
 //index file
 
 function createNewPlace(name, type, address, phone, description, url, callback){
-    //console.log("here");
-    // We'll post to the add-photo endpoint for the appropriate person.
     var postUrl = '/create/';
 
-    // Start a new request to post our newly added photo as JSON data.
     var postRequest = new XMLHttpRequest();
     postRequest.open('POST', postUrl);
     postRequest.setRequestHeader('Content-Type', 'application/json');
 
-    /*
-    * Set up a simple handler for completed requests.  This will send an error
-    * into the callback if we don't get a 200 (success) status back.
-    */
     postRequest.addEventListener('load', function (event) {
         var error;
         if (event.target.status !== 200) {
@@ -22,7 +15,6 @@ function createNewPlace(name, type, address, phone, description, url, callback){
         callback(error);
     });
 
-    // Send our photo data off to the server.
     postRequest.send(JSON.stringify({
         name: name,
         type: type,
@@ -45,23 +37,17 @@ function addNewPlace(){
     //console.log("url " + url);
 
     if(name.trim() && type.trim() && address.trim() && phone.trim() && description.trim() && url.trim() ){
-    //if(url.trim()){
-        //console.log("url " + url);
-        //all fields are there
-        //alert("completed");
         createNewPlace(name, type, address, phone, description, url, function(err){
             if(err){
-                //print error to screen
                 alert("Error:\n" + err);
             } else {
                 //redirect to new page
-                alert("did the thing!");
+                window.setTimeout(function(){ window.location = "http://localhost:3000/place/" + name; },1000);
             }
         });
     } else {
         alert("Please complete all input forms");
     }
-    //alert("done");
 
 }
 
